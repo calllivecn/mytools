6d79d642da31068818948b3fa0b858cbd7efa355
calllivecn <c-all@qq.com>
2017-04-03 23:32:00 +0800

    Changes to be committed: new file:   complie-py.py modified:   findcmp.py renamed:    tar.py -> ztar.py

---
+++ complie-py.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+
+from py_compile import compile as cpe
+from py_compile import PyCompileError
+from compileall import cpeall
+import os,argparse
+
+
+
+
+
+
+
