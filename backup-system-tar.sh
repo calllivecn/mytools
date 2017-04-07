6b783693f232a1a049ea891e177e77b15e16e1c5
calllivecn <c-all@qq.com>
2017-04-07 22:32:57 +0800

    修改完成：     backup-system-tar.sh

--- backup-system-tar.sh
+++ backup-system-tar.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -z "$1" ];then
-	echo "using: $0 <out_file>"
+	echo "using: $(basename $0) <out_file>"
 	exit -1
 fi
 
@@ -17,18 +17,7 @@ user_excludes='--exclude=./home/* --exclude=./mnt/* --exclude=./media/*'
 
 excludes="$sys_excludes $user_excludes"
 
-for arg in $excludes
-do
-	echo $arg
-done
-
-#exit 1
-
-set -x
-
 tar -C / -pc .  ${excludes} |pxz > $out_file
-
-set +x
 
 sha512sum $out_file > ${out_file}.sha512sum
 
