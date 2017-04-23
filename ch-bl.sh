1fa71ff073367f54e261c1d78e6dfc67264cb719
calllivecn <c-all@qq.com>
2017-04-23 16:38:16 +0800

    修改：ch-bl.sh

--- ch-bl.sh
+++ ch-bl.sh
@@ -3,6 +3,22 @@
 # adjustment display brightness for intel_backlight
 # 
 
+case "$1" in
+	-h|--help)
+	echo "adjustment display brightness for intel_backlight"
+	echo
+	echo "Usaing: $(basename $0)"
+	echo
+	echo "'k' or 'u' up adjustment 'j' or ' 'd' down adjustemnt"
+	echo "'q' quit"
+	exit 1
+	;;
+	*)
+	:
+	;;
+esac
+
+
 if [ $(id -u) -ne 0 ];then
 	echo "need the root user."
 	exit 1
@@ -53,12 +69,12 @@ while :
 do
 	
 	ch=$(getch)
-	if [ "$ch"x == "j"x ];then
+	if [ "$ch"x == "j"x -o "$ch"x == "d"x ];then
 		bri_var=$[bri_var - step]
 		bri_var=$(check_overflow $bri_var)
 		ch_bri $bri_var
 		echo "current brightness: $bri_var"
-	elif [ "$ch"x == "k"x ];then
+	elif [ "$ch"x == "k"x -o "$ch"x == "u"x ];then
 		bri_var=$[bri_var + step]
 		bri_var=$(check_overflow $bri_var)
 		ch_bri $bri_var
