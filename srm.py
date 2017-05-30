737343965a924a6353667f55b9428904417a09c0
calllivecn <calllivecn@outlook.com>
2017-05-30 16:09:31 +0800

    update srm.py

--- srm.py
+++ srm.py
@@ -10,14 +10,27 @@ parse.add_argument('files',nargs='+',help='security deletion files')
 args=parse.parse_args()
 
 
+def clear_filename(filename):
+	char = '0'
+	fn_len = len(filename)
+	clear_fn = char * fn_len
+	while os.path.exists(clear_fn):
+		clear_fn += char
+		if len(clear_fn) > 128:
+			char = '1'
+			clear_fn = char * fn_len
+	
+	os.rename(f,clear_fn)
+	os.remove(clear_fn)
 
-l='0'*4096
+
+l=bytes(4096)
 
 for f in args.files:
 	
 	file_size = os.stat(f).st_size
 	count=file_size//4096+1
-	fp = open(f,'r+')
+	fp = open(f,'r+b')
 	
 	for tmp in range(count):
 		fp.write(l)
@@ -25,5 +38,5 @@ for f in args.files:
 		
 	fp.close()
 	
-	os.remove(f)
+	clear_filename(f)
 
