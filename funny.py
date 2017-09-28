bf7be48c2cc2979e0d2b9a9c16bfa6822c02417f
calllivecn <calllivecn@outlook.com>
2017-09-28 15:48:11 +0800

    修改：findcmp.py 将sqlite3的 sha512列的类型改为int型。以减少sqlite数据库占用空间。 修改：funny.py

--- funny.py
+++ funny.py
@@ -11,11 +11,11 @@ S=' '.join(sys.argv[1:])
 def randint(char):
 	len_char = len(char.encode())
 	if len_char <= 1:
-		return random.randint(10,30)/100
+		return random.randint(10,20)/100
 	elif 1 < len_char <=2:
-		return random.randint(30,50)/100
+		return random.randint(20,30)/100
 	else:
-		return random.randint(50,70)/100
+		return random.randint(30,50)/100
 
 
 for char in S:
