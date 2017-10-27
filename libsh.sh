6353bffbb672d52d662f926b37d824d5ba32a83c
calllivecn <calllivecn@outlook.com>
2017-10-27 16:39:16 +0800

    修改：libsh.sh add s_to_date()

--- libsh.sh
+++ libsh.sh
@@ -3,6 +3,20 @@ getdatetime(){
 	date +%Y%m%d%H%M%S
 }
 
+# 和 start=$(date +%s) end=$(date +%s)
+# 配合使用
+#
+s_to_date(){
+	local tmp s="$1" H=3600 M=60 D=86400 h m d 
+	d=$[$s / $D]
+	s=$[$s % $D]
+	h=$[$s / $H]
+	s=$[$s % $H]
+	m=$[$s / $M]
+	s=$[$s % $M]
+	echo "$d-$h:$m:$s"
+}
+
 getch(){
 
 local TTY
