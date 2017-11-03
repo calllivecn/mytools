040cfda85e9f8242a19869c3b4de2517235e6dcd
calllivecn <c-all@qq.com>
2017-11-04 01:58:07 +0800

    修改：ch-bl.sh 升级为 ch-bl.py 修改：libpy/libpy.py

--- libpy/libpy.py
+++ libpy/libpy.py
@@ -1,14 +1,13 @@
 
 
 
-import time
 import datetime
 import sys
-import os
 
 
 
 ## 装饰器
+import time
 def runtime(func):
 	def wapper(*args,**key):
 		start = time.time()
@@ -22,6 +21,7 @@ def runtime(func):
 
 
 
+import os
 def getabspath(f):
 	"""getabspath(file) --> (abspath , filename)"""
 	p = os.path.abspath(f)
@@ -29,8 +29,20 @@ def getabspath(f):
 	return p
 
 
+import termios,os,copy
+def getch():
+	fd = sys.stdin.fileno()
+	old_settings = termios.tcgetattr(fd)
+	new_settings = copy.deepcopy(old_settings)
+	new_settings[3] &= ~(termios.ICANON | termios.ECHO) # | termios.ISIG)
+	#new_settings[6][termios.VMIN] = 1
+	#new_settings[6][termios.VTIME] = 0
+	termios.tcsetattr(fd,termios.TCSADRAIN,new_settings)
+	
+	ch = os.read(fd,8)
 
+	termios.tcsetattr(fd,termios.TCSADRAIN,old_settings)
 
-
+	return ch
 
 
