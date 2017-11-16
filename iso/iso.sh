80b619026a3718b165d4e5d0f38ed70a1d63b1c1
calllivecn <calllivecn@outlook.com>
2017-11-16 08:12:31 +0800

    修改：     iso.sh 修改：     iso_cfg/grub.cfg

--- iso/iso.sh
+++ iso/iso.sh
@@ -14,7 +14,8 @@ ldlinux_c32=/usr/lib/syslinux/modules/bios/ldlinux.c32
 
 xorriso -as mkisofs -o ${2} -no-pad \
     -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
-    -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
+    -c isolinux/boot.cat -b isolinux/isolinux.bin \
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
     -eltorito-alt-boot -e boot/efi.img -no-emul-boot \
     -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \ #-appid "Deepin LiveCD" -publisher "Deepin Project <http://www.deepin.org>" \
     -V "${3}" ${1}
