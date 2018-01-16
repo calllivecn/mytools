cad76ff4a5f97e310e050d838b00eedf5660a1b6
calllivecn <c-all@qq.com>
2018-01-16 20:11:59 +0800

    修改：devel/thunder_parse.py

--- devel/thunder_parse.py
+++ devel/thunder_parse.py
@@ -6,12 +6,21 @@ import sys
 from base64 import encodebytes,decodebytes
 
 thunder1 = sys.argv[1]
+print()
 print('step1:',thunder1)
+print()
 thunder2 = thunder1.lstrip('thunder://')
 print('step2:',thunder2)
-decode3 = decodebytes(thunder2.encode()).decode()
-print('step3:',decode3)
-thunder4 = decode3.lstrip('AA').rstrip('ZZ')
+print()
+decode3 = decodebytes(thunder2.encode())
+try:
+    decode4 = decode3.decode()
+except UnicodeError:
+    decode4 = decode3.decode('gb18030')
+
+print('step3:',decode4)
+print()
+thunder4 = decode4.lstrip('AA').rstrip('ZZ')
 
 print(thunder4)
 
