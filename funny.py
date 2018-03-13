e543d1469acbf81e1dac53de04e29e74e0b4ce7b
calllivecn <calllivecn@outlook.com>
2018-03-13 20:20:18 +0800

    修改：     funny.py

--- funny.py
+++ funny.py
@@ -11,18 +11,20 @@ S=' '.join(sys.argv[1:])
 
 def randint(char):
     len_char = len(char.encode())
+    print(char,len_char,file=sys.stderr)
     if len_char <= 1:
-        return random.randint(10,20)/100
+        return random.randint(10,30)/100
     elif 1 < len_char <=2:
-        return random.randint(20,30)/100
+        return random.randint(20,40)/100
     else:
-        return random.randint(30,50)/100
+        return random.randint(30,60)/100
 
 
 for char in S:
     time.sleep(randint(char))
     print(char,sep='',end='',flush=True)
 
+time.sleep(random.randint(10,20)/100)
 print()
 
 
