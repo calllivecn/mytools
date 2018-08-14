2f32ec8c4973a34784e5b7ba93fff8a2968475a5
calllivecn <calllivecn@outlook.com>
2018-08-14 13:57:12 +0800

    完成 sh/touchpad.sh

--- sh/touchpad.sh
+++ sh/touchpad.sh
@@ -2,17 +2,25 @@
 # date 2018-02-20 18:27:45
 # author calllivecn <c-all@qq.com>
 
+set -e
 
+# 需要你配置，xinput list --name-only 查看你想操作的设备
+DEVICE_NAME=""
 
+if [ -z "${DEVICE_NAME}" ];then
+	echo "需要你配置 ${0##*/} 里面的 DEVICE_NAME 变量，"
+	echo "xinput list --name-only 查看你想操作的设备"
+	exit 2
+fi
 
-STATUS=$(xinput list-props 16 |grep  -i enable |awk -F':' '{print $2}')
+device=$(xinput list --id-only "${DEVICE_NAME}")
 
-STATUS=$(echo $STATUS |tr -d ' ' )
+STATUS=$(xinput list-props $device |grep  -i enable |awk -F':' '{print $2}' |tr -d '\t ' )
 
 if [ "$STATUS"x == "1"x ];then
-	xinput disable 16
+	xinput disable $device
 elif [ "$STATUS"x == "0"x ];then
-	xinput enable 16
+	xinput enable $device
 else
 	echo "$0 status Error : $STATUS"
 fi
