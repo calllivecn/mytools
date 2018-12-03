bad20c45100be2b4d207891bb4b5a7c743ea8e6c
calllivecn <c-all@qq.com>
2018-12-03 17:26:18 +0800

    chmod +x *.py *.sh

---
+++ PS1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# date 2018-11-30 10:15:40
+# author calllivecn <c-all@qq.com>
+
+
+ps1(){
+	local ok_recode='☺ ' err_recode='☹ ' prompt='☘ '
+	if [ $1 -eq 0 ];then
+		recode="$ok_recode"
+	else
+		recode="$err_recode"
+	fi
+	
+	echo "$recode"
+}
+
+PS1="$(ps1 $?)-\u@\h:\W☘ "
