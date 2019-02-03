7a5aab5efb0da22182903e61de1586b3f29ac7e3
calllivecn <calllivecn@outlook.com>
2019-02-03 10:18:04 +0800

    修改：ssh-tunnel.sh

--- ssh-tunnel.sh
+++ ssh-tunnel.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-# date 2019-01-20 15:05:01
+# date 2019-02-03 09:39:18
 # author calllivecn <c-all@qq.com>
+# https://github.com/calllivecn
 
 
-ssh -N -L 2222:
+t=0
+while :;
+do
+
+	ssh -N -R 1222:localhost:22 linux@calllive.cc && t=0
+	
+	if [ $t -gt 5 ];then
+		:
+	else
+		t=$[$t + 1]
+	fi
+
+	sleep $[ $t * 60 ]
+done
+
