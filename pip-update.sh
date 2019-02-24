42e05e10ffb46198c0f53fdd09e6df5798f2b4da
calllivecn <calllivecn@outlook.com>
2019-02-24 14:40:44 +0800

    新文件：pip-update.sh

---
+++ pip-update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# date 2019-02-24 14:28:22
+# author calllivecn <c-all@qq.com>
+
+
+PIP="pip3"
+
+manual_installed_packages=$($PIP list --not-required --format=freeze |awk -F'=' '{print $1}')
+
+for pack in $manual_installed_packages
+do
+
+	if [ -n $VIRTUAL_ENV ];then
+		$PIP install --upgrade "$pack"
+	elif [ $EUID -ne 0 ];then
+		sudo $PIP install --upgrade "$pack"
+	elif [ $UID -eq -];then
+		$PIP install --upgrade "$pack"
+	else
+		echo "你这好像不行啊。"
+	fi
+
+done
