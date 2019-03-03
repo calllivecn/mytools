bb7bee5a90a062ce22860c5016c33861e22ba395
calllivecn <calllivecn@outlook.com>
2019-03-03 20:13:18 +0800

    修改：aes.py
    改用openssl，libctypro.so C库，速度提升到230M/s。

--- aes.py
+++ aes.py
@@ -9,11 +9,19 @@ import time
 import getpass
 import logging
 import argparse
+
+import glob
+import random
+
 from hashlib import sha256
 from binascii import b2a_hex
 from os.path import split,join,isfile,isdir,exists,abspath,islink
 from struct import Struct, pack, unpack
 from threading import Thread
+
+from ctypes import CDLL
+from ctypes import c_char_p, c_int, c_long, byref, create_string_buffer, c_void_p
+from ctypes.util import find_library
 
 
 try:
@@ -22,7 +30,13 @@ except ModuleNotFoundError:
     print("请运行pip install pycrypto 以安装依赖")
     sys.exit(2)
 
+
+CIPHER = 1 # 加密
+DECIPHER = 0 # 解密
+
 FILE_VERSION = 0x01
+
+
 
 def getlogger(level=logging.INFO):
     fmt = logging.Formatter("%(asctime)s %(filename)s:%(lineno)d %(message)s",datefmt="%Y-%m-%d-%H:%M:%S")
@@ -37,6 +51,229 @@ logger = getlogger()
 
 class PromptTooLong(Exception):
     pass
+
+
+####################################
+#
+# 加载openssl函数库 begin
+#
+##################################
+
+
+# 加密类
+class OpenSSLCrypto:
+    ciphers = {
+        'aes-128-cbc': (16, 16),
+        'aes-192-cbc': (24, 16),
+        'aes-256-cbc': (32, 16),
+        'aes-128-cfb': (16, 16),
+        'aes-192-cfb': (24, 16),
+        'aes-256-cfb': (32, 16),
+        'aes-128-ofb': (16, 16),
+        'aes-192-ofb': (24, 16),
+        'aes-256-ofb': (32, 16),
+        'aes-128-ctr': (16, 16),
+        'aes-192-ctr': (24, 16),
+        'aes-256-ctr': (32, 16),
+        'aes-128-cfb8': (16, 16),
+        'aes-192-cfb8': (24, 16),
+        'aes-256-cfb8': (32, 16),
+        'aes-128-cfb1': (16, 16),
+        'aes-192-cfb1': (24, 16),
+        'aes-256-cfb1': (32, 16),
+        'bf-cfb': (16, 8),
+        'camellia-128-cfb': (16, 16),
+        'camellia-192-cfb': (24, 16),
+        'camellia-256-cfb': (32, 16),
+        'cast5-cfb': (16, 8),
+        'des-cfb': (8, 8),
+        'idea-cfb': (16, 8),
+        'rc2-cfb': (16, 8),
+        'rc4': (16, 0),
+        'seed-cfb': (16, 16),
+    }
+
+    def __init__(self, cipher_name, key, iv, op):
+        self.libcrypto = None
+        self.loaded = False
+        self.buF = b''
+        self.buf_size = 8192 # 1<<13, 8k size
+
+        self._ctx = None
+
+        if not self.loaded:
+            self.__load_openssl()
+        cipher = self.libcrypto.EVP_get_cipherbyname(cipher_name.encode("utf-8"))
+        if not cipher:
+            cipher = self.__load_cipher(cipher_name)
+        if not cipher:
+            raise Exception('cipher {} not found in libcrypto'.format(cipher_name))
+        key_ptr = c_char_p(key)
+        iv_ptr = c_char_p(iv)
+        self._ctx = self.libcrypto.EVP_CIPHER_CTX_new()
+        if not self._ctx:
+            raise Exception('can not create cipher context')
+        r = self.libcrypto.EVP_CipherInit_ex(self._ctx, cipher, None,
+                                        key_ptr, iv_ptr, c_int(op))
+        if not r:
+            self.__clean()
+            raise Exception('can not initialize cipher context')
+
+    def update(self, data):
+        #global self.buf_size, self.buf
+
+        cipher_out_len = c_long(0)
+        l = len(data)
+        if self.buf_size < l:
+            self.buf_size = l * 2
+            self.buf = create_string_buffer(self.buf_size)
+        self.libcrypto.EVP_CipherUpdate(self._ctx, byref(self.buf),
+                                   byref(cipher_out_len), c_char_p(data), l)
+        # self.buf is copied to a str object when we access self.buf.raw
+        return self.buf.raw[:cipher_out_len.value]
+
+
+    def __load_cipher(self, cipher_name):
+        func_name = 'EVP_' + cipher_name.replace('-', '_')
+        cipher = getattr(LIBCRYPTO, func_name, None)
+        if cipher:
+            cipher.restype = c_void_p
+            return cipher()
+        return None
+    
+    def __rand_bytes(self, length):
+        if not self.loaded:
+            self.__load_openssl()
+        self.buf = create_string_buffer(length)
+        r = LIBCRYPTO.RAND_bytes(self.buf, length)
+        if r <= 0:
+            raise Exception('RAND_bytes return error')
+        return self.buf.raw
+
+    def __find_library_nt(self, name):
+        # modified from ctypes.util
+        # ctypes.util.find_library just returns first result he found
+        # but we want to try them all
+        # because on Windows, users may have both 32bit and 64bit version installed
+        results = []
+        for directory in os.environ['PATH'].split(os.pathsep):
+            fname = os.path.join(directory, name)
+            if os.path.isfile(fname):
+                results.append(fname)
+            if fname.lower().endswith(".dll"):
+                continue
+            fname = fname + ".dll"
+            if os.path.isfile(fname):
+                results.append(fname)
+        return results
+    
+    
+    def __find_library_notnt(self, possible_lib_names, search_symbol, library_name):
+        # move to file head.
+        #import ctypes.util
+        #from ctypes import CDLL
+    
+        paths = []
+    
+        if type(possible_lib_names) not in (list, tuple):
+            possible_lib_names = [possible_lib_names]
+    
+        lib_names = []
+        for lib_name in possible_lib_names:
+            lib_names.append(lib_name)
+            lib_names.append('lib' + lib_name)
+    
+        for name in lib_names:
+            if os.name == "nt":
+                paths.extend(self.__find_library_nt(name))
+            else:
+                path = find_library(name)
+                if path:
+                    paths.append(path)
+    
+        if not paths:
+            # We may get here when find_library fails because, for example,
+            # the user does not have sufficient privileges to access those
+            # tools underlying find_library on linux.
+    
+            #move to file head.
+            #import glob 
+    
+            for name in lib_names:
+                patterns = [
+                    f'/usr/local/lib*/lib{name}.*',
+                    f'/usr/lib*/lib{name}.*',
+                    f'lib{name}.*',
+                    f'{name}.dll']
+    
+                for pat in patterns:
+                    files = glob.glob(pat)
+                    if files:
+                        paths.extend(files)
+        for path in paths:
+            try:
+                lib = CDLL(path)
+                if hasattr(lib, search_symbol):
+                    logging.info('loading {} from {}'.format(library_name, path))
+                    return lib
+                else:
+                    logging.warn('can\'t find symbol {} in {}'.format(search_symbol, path))
+            except Exception:
+                if path == paths[-1]:
+                    raise
+        return None
+    
+
+    def __load_openssl(self):
+        #global LOADED, LIBCRYPTO, self.buf
+    
+        self.libcrypto = self.__find_library_notnt(('crypto', 'eay32'),
+                                      'EVP_get_cipherbyname',
+                                      'libcrypto')
+        if self.libcrypto is None:
+            raise Exception('libcrypto(OpenSSL) not found')
+    
+        self.libcrypto.EVP_get_cipherbyname.restype = c_void_p
+        self.libcrypto.EVP_CIPHER_CTX_new.restype = c_void_p
+    
+        self.libcrypto.EVP_CipherInit_ex.argtypes = (c_void_p, c_void_p, c_char_p,
+                                                c_char_p, c_char_p, c_int)
+    
+        self.libcrypto.EVP_CipherUpdate.argtypes = (c_void_p, c_void_p, c_void_p,
+                                               c_char_p, c_int)
+    
+        if hasattr(self.libcrypto, "EVP_CIPHER_CTX_cleanup"):
+            self.libcrypto.EVP_CIPHER_CTX_cleanup.argtypes = (c_void_p,)
+        else:
+            self.libcrypto.EVP_CIPHER_CTX_reset.argtypes = (c_void_p,)
+        self.libcrypto.EVP_CIPHER_CTX_free.argtypes = (c_void_p,)
+    
+        self.libcrypto.RAND_bytes.restype = c_int
+        self.libcrypto.RAND_bytes.argtypes = (c_void_p, c_int)
+    
+        if hasattr(self.libcrypto, 'OpenSSL_add_all_ciphers'):
+            self.libcrypto.OpenSSL_add_all_ciphers()
+    
+        self.buf = create_string_buffer(self.buf_size)
+        self.loaded = True
+
+    def __del__(self):
+        self.__clean()
+
+    def __clean(self):
+        if self._ctx:
+            if hasattr(self.libcrypto, "EVP_CIPHER_CTX_cleanup"):
+                self.libcrypto.EVP_CIPHER_CTX_cleanup(self._ctx)
+            else:
+                self.libcrypto.EVP_CIPHER_CTX_reset(self._ctx)
+            self.libcrypto.EVP_CIPHER_CTX_free(self._ctx)
+
+
+####################################
+#
+# 加载openssl函数库 end
+#
+####################################
 
 class FileFormat:
     """
@@ -194,14 +431,18 @@ def main():
         header.setHeader(out_stream)
 
         key = salt_key(password, header.salt)
-        aes = AES.new(key, AES.MODE_CFB, header.iv)
+        #aes = AES.new(key, AES.MODE_CFB, header.iv)
+        aes = OpenSSLCrypto("aes-256-cfb", key, header.iv, CIPHER)
+        #aes = OpenSSLCrypto("aes-256-cfb1", key, header.iv, CIPHER) # 这个好慢？？？
+        #aes = OpenSSLCrypto("aes-256-cfb8", key, header.iv, CIPHER)
 
         data = True
         while data:
 
             data = in_stream.read(block)
 
-            en_data = aes.encrypt(data)
+            #en_data = aes.encrypt(data)
+            en_data = aes.update(data)
 
             out_stream.write(en_data)
 
@@ -218,13 +459,17 @@ def main():
         file_version, prompt_len, iv, salt, prompt = header.getHeader(in_stream)
 
         key = salt_key(password, salt)
-        aes = AES.new(key, AES.MODE_CFB, iv)
+        #aes = AES.new(key, AES.MODE_CFB, iv)
+        aes = OpenSSLCrypto("aes-256-cfb", key, iv, DECIPHER)
+        #aes = OpenSSLCrypto("aes-256-cfb1", key, iv, DECIPHER) # 这个好慢？？？
+        #aes = OpenSSLCrypto("aes-256-cfb8", key, iv, DECIPHER)
 
         data = True
         while data:
 
             data = in_stream.read(block)
-            de_data = aes.decrypt(data)
+            #de_data = aes.decrypt(data)
+            de_data = aes.update(data)
             out_stream.write(de_data)
         
 
