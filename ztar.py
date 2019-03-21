d8e39164d516758b647cd7b20efa8836822c09a5
calllivecn <calllivecn@outlook.com>
2019-03-21 18:51:53 +0800

    修改：ztar.py

--- ztar.py
+++ ztar.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 #coding=utf-8
 
+import sys
+import os
+from os import path
+import argparse
+import tarfile
 
-import tarfile,sys,os,argparse
 
 parse=argparse.ArgumentParser(
 description=r'''GNU %(prog)s saves many files together into a single tape or disk archive,
@@ -12,19 +16,23 @@ Examples:
   tar -cf archive.tar foo bar  # Create archive.tar from files foo and bar.
   tar -tvf archive.tar         # List all files in archive.tar verbosely.
   tar -xf archive.tar          # Extract all files from archive.tar.
-''',usage='%(prog)s [option...] [FILE]...'
+''',
+usage='%(prog)s [option...] [FILE]...',
+epilog="\n",
 )
 
 
-parse.add_argument('-c','--create',action='store_true',help='create a new archive')
+group1 = parse.add_mutually_exclusive_group()
 
-parse.add_argument('-x','--extract',action='store_true',help='extract files from an archive')
+group1.add_argument('-c','--create',action='store_true',help='create a new archive')
 
-parse.add_argument('-t','--list',action='store_true',help='list the contents of an archive')
+group1.add_argument('-x','--extract',action='store_true',help='extract files from an archive')
+
+group1.add_argument('-t','--list',action='store_true',help='list the contents of an archive')
 
 parse.add_argument('-f','--file',action='store',required=True,help='use archive file or device ARCHIVE')
 
-parse.add_argument('-v','--verbose',action='count',help='verbosely list files processed')
+parse.add_argument('-v','--verbose',action='count', default=0,help='verbosely list files processed')
 
 parse.add_argument('files',nargs='*',help='arvchive file or directory')
 
@@ -41,13 +49,29 @@ groups.add_argument('-J','--xz',dest='xz',action='store_true',help='filter the a
 #parse.add_argument('--exclude',nargs='*',help='exclude files, given as a PATTERN')
 
 
-args=parse.parse_args()
+args = parse.parse_args()
 
-#print(args)
+print(args)
 
-if args.create and args.files :
+def compress_safe_path(filename):
+    # 它会自动去掉路径，/ 和C:\这样的开头。
+    realpath = path.realpath(filename)
+    _, memberpath = path.split(realpath)
+    dirname = path.dirname(realpath)
+
+    return memberpath
+
+
+def decompress_safe_path(filename):
+    realpath = path.realpath(filename)
+    return realpath
+
+if args.create or args.extract:
+    os.chdir(args.directory)
+
+if args.create and args.files:
     compress='w'
-    
+
     if args.gzip:
         compress='w:gz'
     elif args.bzip2:
@@ -55,23 +79,15 @@ if args.create and args.files :
     elif args.xz:
         compress='w:xz'
 
-    fp=tarfile.open(args.file,compress)
+    fp = tarfile.open(args.file, compress, debug=args.verbose)
     for f in args.files:
-        if os.path.isfile(f):
-            if args.verbose: print(f)
-            fp.add(f)
-        elif os.path.isdir(f):
-            for root,dirs,files in os.walk(f):
-                for file2 in files:
-                    if args.verbose: print(os.path.join(root,file2))
-                    fp.add(os.path.join(root,file2))
-        else:
-                print(f,': is not file or directory')
-                continue
-    fp.close()            
-            
+        memberpath = compress_safe_path(f)
+        fp.add(f, memberpath)
+        #print("add({}, {})".format(f, memberpath))
 
-elif args.extract :
+    fp.close()         
+
+elif args.extract:
     decompress='r'
     
     if args.gzip:
@@ -81,22 +97,15 @@ elif args.extract :
     elif args.xz:
         decompress='r:xz'
 
-    fp=tarfile.open(args.file,decompress)
-    for f in fp.getnames():
-        if os.path.isdir(args.directory):
-            if args.verbose:
-                print(f)
-            fp.extract(f,args.directory)
-        elif args.directory==None :
-            if args.verbose:
-                print(f)
-            fp.extract(f,'.')
-        else:
-            print(args.directory,'not a directory, exited')
-            sys.exit(2)
+    fp=tarfile.open(args.file,decompress, debug=args.verbose)
+
+    #fp.extract(f,args.directory)
+    safe_path = decompress_safe_path(
+    fp.extractall()
+
     fp.close()
 
-elif args.list :
+elif args.list:
     decompress='r'
     
     if args.gzip:
@@ -106,7 +115,7 @@ elif args.list :
     elif args.xz:
         decompress='r:xz'
 
-    fp=tarfile.open(args.file,decompress)
+    fp=tarfile.open(args.file, decompress, debug=args.verbose)
 
     fp.list(verbose=args.verbose)
 
