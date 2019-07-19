b81b752ae31ca7a18f0976605a0be1deccbbce4c
calllivecn <calllivecn@outlook.com>
2019-07-19 22:38:46 +0800

    修改：wakeonlan.py 添加 wake on LAN 协议工作原理说明。

--- wakeonlan.py
+++ wakeonlan.py
@@ -5,32 +5,29 @@
 
 
 """
-Small module for use with the wake on lan protocol.
+wakeOnLAN工作原理：
+    0. 前提：现在绝大多数的主板和网卡都是直接支持的。
+    1. 在BIOS里设置wake on LAN 开启(的有主板还可以设置一个4或6字节的密码)。
+    2. 接电源，插网线。（如果：工作了，网卡的指示灯一般会亮起。也有可以网卡没有指示灯。。。）
+
+
+Wake On LAN 协议实现：
+    0. 魔法数据包（Magic Packet）是一个广播性的帧（frame），透过端口7或端口9进行发送，且可以用无连接（Connectionless protocol）的通信协议（如UDP、IPX）来传递，不过一般而言多是用UDP。
+    1. 在魔法数据包内，每次都会先有连续6个"FF"（十六进制，换算成二进制即：11111111）的数据，即：FF FF FF FF FF FF，在连续6个"FF"后则开始带出MAC地址信息，有时还会带出4字节或6字节的密码，一旦经由网卡侦测、解读、研判（广播）魔法数据包的内容，内容中的MAC地址、密码若与计算机自身的地址、密码吻合，就会引导唤醒、引导的程序。
 
 """
 
-import argparse
 import socket
-import struct
+import argparse
+from struct import pack
 
 
 BROADCAST_IP = '255.255.255.255'
-DEFAULT_PORT = 9 # 9 or 0 or 7
+DEFAULT_PORT = 9 # 9 or 7
 
 
-def create_magic_packet(macaddress):
-    """
-    Create a magic packet.
+def magic_packet(macaddress):
 
-    A magic packet is a packet that can be used with the for wake on lan
-    protocol to wake up a computer. The packet is constructed from the
-    mac address given as a parameter.
-
-    Args:
-        macaddress (str): the mac address that should be parsed into a
-            magic packet.
-
-    """
     if len(macaddress) == 12:
         pass
     elif len(macaddress) == 17:
@@ -45,26 +42,12 @@ def create_magic_packet(macaddress):
 
     # Split up the hex values in pack
     for i in range(0, len(data), 2):
-        send_data += struct.pack(b'B', int(data[i: i + 2], 16))
+        send_data += pack(b'B', int(data[i: i + 2], 16))
     return send_data
 
 
-def send_magic_packet(*macs, **kwargs):
-    """
-    Wake up computers having any of the given mac addresses.
+def send_packet(*macs, **kwargs):
 
-    Wake on lan must be enabled on the host device.
-
-    Args:
-        macs (str): One or more macaddresses of machines to wake.
-
-    Keyword Args:
-        ip_address (str): the ip address of the host to send the magic packet
-                     to (default "255.255.255.255")
-        port (int): the port of the host to send the magic packet to
-               (default 9)
-
-    """
     packets = []
     ip = kwargs.pop('ip_address', BROADCAST_IP)
     port = kwargs.pop('port', DEFAULT_PORT)
@@ -73,7 +56,7 @@ def send_magic_packet(*macs, **kwargs):
                         'argument {!r}'.format(k))
 
     for mac in macs:
-        packet = create_magic_packet(mac)
+        packet = magic_packet(mac)
         packets.append(packet)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -85,19 +68,17 @@ def send_magic_packet(*macs, **kwargs):
 
 
 def main():
-    """
-    Run wake on lan as a CLI application.
 
-    """
     parser = argparse.ArgumentParser(description='Wake one or more computers using the wake on lan protocol.')
-    parser.add_argument('macs', metavar='mac address', nargs='+', help='The mac addresses or of the computers you are trying to wake.')
     parser.add_argument('-i', metavar='ip', default=BROADCAST_IP, help='The ip address of the host to send the magic packet to.(default {})'.format(BROADCAST_IP))
     parser.add_argument('-p', metavar='port', type=int, default=DEFAULT_PORT, help='The port of the host to send the magic packet to (default 9)')
 
+    parser.add_argument('macs', metavar='mac address', nargs='+', help='The mac addresses or of the computers you are trying to wake.')
+
     args = parser.parse_args()
 
-    send_magic_packet(*args.macs, ip_address=args.i, port=args.p)
+    send_packet(*args.macs, ip_address=args.i, port=args.p)
 
 
-if __name__ == '__main__':  # pragma: nocover
+if __name__ == '__main__':
     main()
