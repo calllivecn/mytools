c720a4680df0f6c7da8d10e5b3d38e3a1c2e93fa
calllivecn <calllivecn@outlook.com>
2019-08-09 11:26:38 +0800

    修改：apt-proxy.sh

--- apt-proxy.sh
+++ apt-proxy.sh
@@ -3,8 +3,26 @@
 # author calllivecn <c-all@qq.com>
 
 
-proxy_addr="http://127.0.0.1:9999"
+PROXY_ADDR="http://127.0.0.1:9999"
+
+URLs="ppa.launchpad.net
+linux.teamviewer.com
+packages.microsoft.com"
+
+APT_OPTION=""
+for http in $URLs
+do
+	for scheme in http https
+	do
+		APT_OPTION="$APT_OPTION -o Acquire::${scheme}::Proxy::${http}=$PROXY_ADDR "
+	done
+done
 
 
-apt -o Acquire::http::Proxy::ppa.launchpad.net="$proxy_addr" "$@"
+if [ "$1"x = "debug"x ];then
+	shift
+	echo apt $APT_OPTION "$@"
+else
+	apt $APT_OPTION "$@"
+fi
 
