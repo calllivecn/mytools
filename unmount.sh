d1547979fbfd37d88a70bf31d16b2a06013b6412
calllivecn <calllivecn@outlook.com>
2019-10-13 14:10:54 +0800

    修改：unmount.sh

--- unmount.sh
+++ unmount.sh
@@ -3,6 +3,7 @@
 # author calllivecn <c-all@qq.com>
 
 
+PROGRAM="${0##*/}"
 
 if type -p udisksctl >/dev/null;then
 	:
@@ -11,4 +12,29 @@ else
 	exit 1
 fi
 
+usage(){
 
+	echo "Usage: $PROGRAM <block device>"
+	echo "例如："
+	echo "$PROGRAM /dev/sdb"
+}
+
+case "$1" in 
+	-h|--help)
+		usage
+		exit 0
+		;;
+esac
+
+if [ -z "$1" ];then
+	echo "${1} 块设备不存在。"
+	exit 2
+fi
+
+
+for part in $(echo "${1}[0-9]*")
+do
+	udisksctl unmount -b "$part"
+done
+
+udisksctl power-off -b "${1}"
