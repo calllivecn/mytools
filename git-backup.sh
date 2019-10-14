d41fc250a9d6cbffeb782979014a7b3f66c730d5
calllivecn <calllivecn@outlook.com>
2019-10-14 10:40:57 +0800

    新文件：git-backup.sh

---
+++ git-backup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# date 2019-10-14 10:13:40
+# author calllivecn <c-all@qq.com>
+
+
+PROGRAM=${0##*/}
+
+usage(){
+
+	echo "Usage: $PROGRAM [目录]"
+	echo "默认执行：${ROGRAM} $HOME/git-backup"
+}
+
+
+
+case "$1" in
+	-h|--help)
+		uasge
+		exit 0
+		;;
+esac
+
+if [ -n "$1" ];then
+	GIT="$1"
+else
+	GIT="$HOME/git-backup"
+	echo "创建 $GIT"
+fi
+
+
+
+if [ -d "$GIT" ];then
+	:
+else
+	echo "$GIT 不存在，请创建 $GIT"
+	echo "并将仓库目录放入${GIT}目录下"
+	exit 1
+fi
+
+for gitdir in $(echo "$GIT/*");
+do
+	if [ -d "$gitdir/.git" ];then
+		pushd "$gitdir"
+		git fetch 
+		popd
+	fi
+done
+
