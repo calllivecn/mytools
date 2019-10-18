88b5ea3c84712e73a951d283aacdfccdb73fac8b
calllivecn <calllivecn@outlook.com>
2019-10-18 17:59:22 +0800

    修改：     unmount.sh
    修改：     wakeonlan.py
    没修改完。

--- wakeonlan.py
+++ wakeonlan.py
@@ -15,6 +15,8 @@ Wake On LAN 协议实现：
     0. 魔法数据包（Magic Packet）是一个广播性的帧（frame），透过端口7或端口9进行发送，且可以用无连接（Connectionless protocol）的通信协议（如UDP、IPX）来传递，不过一般而言多是用UDP。
     1. 在魔法数据包内，每次都会先有连续6个"FF"（十六进制，换算成二进制即：11111111）的数据，即：FF FF FF FF FF FF，在连续6个"FF"后则开始带出MAC地址信息，有时还会带出4字节或6字节的密码，一旦经由网卡侦测、解读、研判（广播）魔法数据包的内容，内容中的MAC地址、密码若与计算机自身的地址、密码吻合，就会引导唤醒、引导的程序。
 
+IPv6 ff02::1组播：
+    0. ipv6 组播地址
 """
 
 import socket
@@ -22,7 +24,9 @@ import argparse
 from struct import pack
 
 
-BROADCAST_IP = '255.255.255.255'
+BROADCAST_IPv4 = '255.255.255.255'
+LOCAL_LINK_IPv6 = 'ff02::1'
+
 DEFAULT_PORT = 9  # 9 or 7
 
 
@@ -46,24 +50,20 @@ def magic_packet(macaddress):
     return send_data
 
 
-def send_packet(*macs, **kwargs):
-
-    packets = []
-    ip = kwargs.pop('ip_address', BROADCAST_IP)
-    port = kwargs.pop('port', DEFAULT_PORT)
-    for k in kwargs:
-        raise TypeError('send_magic_packet() got an unexpected keyword '
-                        'argument {!r}'.format(k))
+def send_packet(macs, ipv4=True):
 
     for mac in macs:
         packet = magic_packet(mac)
-        packets.append(packet)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.connect((ip, port))
-    for packet in packets:
-        sock.send(packet)
+        print(packet)
+
+        if ipv4:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(packet, (BROADCAST_IPv4, DEFAULT_PORT))
+        else:
+            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            sock.sendto(packet, (LOCAL_LINK_IPv6, DEFAULT_PORT))
+
     sock.close()
 
 
@@ -71,8 +71,9 @@ def main():
 
     parser = argparse.ArgumentParser(
         description='Wake one or more computers using the wake on lan protocol.')
-    parser.add_argument('-i', metavar='ip', default=BROADCAST_IP,
-                        help='The ip address of the host to send the magic packet to.(default {})'.format(BROADCAST_IP))
+
+    parser.add_argument('-i','--ipv46' , action="store_false", default=True, help="ipv4 or ipv6 default: ipv4")
+
     parser.add_argument('-p', metavar='port', type=int, default=DEFAULT_PORT,
                         help='The port of the host to send the magic packet to (default 9)')
 
@@ -81,7 +82,7 @@ def main():
 
     args = parser.parse_args()
 
-    send_packet(*args.macs, ip_address=args.i, port=args.p)
+    send_packet(args.macs, args.ipv46)
 
 
 if __name__ == '__main__':
