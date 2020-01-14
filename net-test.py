34b88e1bbc0ee966f2145e3920db1f359d8521ea
calllivecn <calllivecn@outlook.com>
2020-01-14 18:11:17 +0800

    save... 修改：     net-test.py

--- net-test.py
+++ net-test.py
@@ -17,69 +17,98 @@ class Nettest:
     def __init__(self, address, packsize, count, time_, tcp=True):
         self.port = 6789
         self.address = address
-        self.packsize = packsize * 1024
+        self.packsize = packsize
         self.count = count
-        self._time = time_
+        self.time = time_
+        self.tcp = tcp
 
-        self._data = "-" * self.packsize
+        self._datapack = b"-" * self.packsize
+
+        self._recv = b"recv"
+        self._send = b"send"
 
         if tcp:
             self._sock = socket.socket()
+            self._recv = self._sock.recv
+            self._send = self._sock.send
         else:
             self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+            self._recv = self._sock.recvfrom
+            self._send = lambda x: self._sock.sendto((self.address, self.port), x)
         
 
     def testing(self):
 
         conn = self._sock.connect((self.address, self.port))
 
+    def server(self):
+        
+
+    def __server_tcp(self):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.bind((self.address, self.port))
+        self._sock.listen(5)
+
+        while True:
+            print("TCP: {self.address}:{self.port} 等待连接...")
+            client, addr = self._sock.accept()
+            print(f"conn: {addr}")
+            self.__select_rs(client)
+
+    def __server_udp(self):
+
+        while True:
+            print("UDP: {self.address}:{self.port} 等待连接...")
+            self.__select_rs(self._sock)
+
+    def __send(self):
+
         print("测试发送。。。")
         c = 0 
         for _ in range(self.count):
             start = time.time()
-            socke.send((server, SERVER_PORT), self._data)
+            self._send(self._datapack)
             end = time.time()
             c += 1
             t = end - start
             if 1 <= t:
-                print("发送速度：{}KB/s ".format(c * self.packsize))
+                print("接收速度：{} pack/s {} KB/s ".format(c, c * self.packsize))
+                c = 0
+            else:
+                print("接收速度：{} pack/s {} KB/s ".format(c, c * self.packsize / t))
                 c = 0
 
+    def __recv(self):
 
         print("测试接收。。。")
         c = 0 
         for _ in range(self.count):
             start = time.time()
-            self._sock.readfrom((server, SERVER_PORT), self._data)
+            self._recv(self.packsize)
             end = time.time()
             c += 1
             t = end - start
             if 1 <= t:
-                print("接收速度：{}KB/s ".format(c * self.packsize / 1024))
+                print("接收速度：{} pack/s {} KB/s ".format(c, c * self.packsize))
                 c = 0
             else:
-                print("接收速度：{}KB/s ".format(c * self.packsize / 1024 / t))
+                print("接收速度：{} pack/s {} KB/s ".format(c, c * self.packsize / t))
                 c = 0
 
+    def __select_rs(self, conn):
 
-    def server(self, address="0.0.0.0", port=6789):
-        
-        self._sock.bind((address, port))
+        data = conn.read(4)
 
-        self._sock.listen(5)
+        if self._recv == data:
+            self.recv()
+        elif self._send == data:
+            self.send()
+        else:
+            print(f"错误的标识头：{data}，关闭连接。")
+            client.close()
 
-        while True:
-            client = self._sock.accept()
-
-            if
-            while True:
-                data = sock._sock.recv(self.packsize)
-                self._sock.send(data)
-
-        
     def close(self):
-
         self._sock.close()
 
 
@@ -90,15 +119,19 @@ def main():
             )
     tcp_udp = parse.add_mutually_exclusive_group()
     tcp_udp.add_argument("-t", "--tcp", action="store_true", default=True, help="使用TCP(default)")
-    #tcp_udp.add_argument("-u", "--udp", action="store_false", default=False, help="使用UDP")
+    tcp_udp.add_argument("-u", "--udp", action="store_false", default=False, help="使用UDP")
 
     time_count = parse.add_mutually_exclusive_group()
-    time_count.add_argument("-T", "--time", type=float, default=15.0, help="测试持续时间。(单位：秒，默认15。0: 一直测试。)")
+    time_count.add_argument("--time", type=float, default=15.0, help="测试持续时间。(单位：秒，默认15，0: 一直测试。)")
     time_count.add_argument("-c", "--count", type=int, default=1000, help="发送的数据包数量(default: 1000)")
 
-    parse.add_argument("-s", "--size", type=int, default=1, help="发送数据包大小(default: 1K)")
+    parse.add_argument("-s", "--size", type=int, default=1024, help="发送数据包大小(default: 1024 byte)")
 
-    #parse.add_argument("--recv", action="store_true", help="只接收数据")
+    parse.add_argument("--server", action="store_true", help="启动测试server。")
+
+    parse.add_argument("--recv", action="store_true", help="测试接收数据")
+
+    parse.add_argument("--send", action="store_true", help="测试发送数据")
 
     parse.add_argument("host", required=True, help="target address")
 
