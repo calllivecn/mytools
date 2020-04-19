b3140fe63adcc46de777f42b8d7d97d1aa99534b
calllivecn <calllivecn@outlook.com>
2020-04-19 11:22:52 +0800

    修改：     aes.py 添加 libcrypto.so 加密使用流程说明

--- aes.py
+++ aes.py
@@ -9,22 +9,42 @@ import time
 import getpass
 import logging
 import argparse
-
 import glob
-import random
-
 from hashlib import sha256
 from binascii import b2a_hex
 from functools import partial
-from os.path import split, join, isfile, isdir, exists, abspath, islink
-from struct import Struct, pack, unpack
 from threading import Thread
 
+from os.path import (
+                    split, 
+                    join, 
+                    isfile, 
+                    isdir, 
+                    exists, 
+                    abspath, 
+                    islink
+                )
+
+from struct import (
+                    Struct, 
+                    pack, 
+                    unpack
+                )
+
 from ctypes import CDLL
-from ctypes import c_char_p, c_int, c_long, byref, create_string_buffer, c_void_p
 from ctypes.util import find_library
+from ctypes import (
+                    c_char_p, 
+                    c_int, 
+                    c_long, 
+                    byref, 
+                    create_string_buffer, 
+                    c_void_p
+                )
 
 """
+# 这玩意的加密速度太慢了。
+# 改用openssl，libctypro.so C库，速度提升到230M/s。
 try:
     from Crypto.Cipher import AES
 except ModuleNotFoundError:
@@ -67,6 +87,109 @@ class PromptTooLong(Exception):
 
 # 加密类
 class OpenSSLCrypto:
+    """
+    对称加密和解密的流程类似，一般有以下几个步骤：
+
+    1. 生成一个记录加密（解密）上下文信息的EVP_CIPHER_CTX对象
+    2. 初始化加密（解密）算法，在这一步指定算法和密钥
+    3. 加密（解密）数据
+    4. 处理尾部数据，结束加密（解密）
+    5. 清空并释放加密（解密）上下文对象，清空其他敏感信息
+    其中使用的函数以及其他一些相关函数如下：
+
+    1. 创建新加密上下文EVP_CIPHER_CTX对象, 并将其作为返回值返回:
+        EVP_CIPHER_CTX *EVP_CIPHER_CTX_new(void)
+    
+    2. 清除并释放加密上下文对象(防止数据泄露)，参数为需要释放的EVP_CIPHER_CTX对象，在所有加密操作结束后调用该函数:
+        void EVP_CIPHER_CTX_free(EVP_CIPHER_CTX *ctx)
+
+    3. 目前不是很清楚具体作用，可能是重置一个EVP_CIPHER_CTX对象从而可以循环利用避免不必要的内存释放和分配吧:
+        int EVP_CIPHER_CTX_reset(EVP_CIPHER_CTX *ctx)
+
+    4. 加解密初始化操作
+
+        4.1. 执行加密初始化
+
+        int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl, const unsigned char *in, int inl)
+        返回值为1表示成功，0表示失败，可以使用上述错误处理中的函数打印错误信息
+
+        参数	描述
+        ctx	    加密上下文对象
+        out	    保存输出结果（密文）的缓冲区
+        outl	接收输出结果长度的指针
+        in	    包含输入数据（明文）的缓冲区
+        inl	    输入数据的长度
+
+        4.2. 执行解密初始化
+        int EVP_DecryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type, ENGINE *impl, const unsigned char *key, const unsigned char *iv)
+        该函数对解密操作进行初始化，参数与返回值上述加密初始化函数描述相同
+
+        参数    描述
+        ctx	    加密上下文对象
+        out	    保存输出结果（密文）的缓冲区（注意这个指针要指向之前已经保存的加密数据的尾部）
+        outl	接收输出结果长度的指针
+
+    5. 执行加解密操作
+        注意, 输出缓冲区的长度需要比输入缓冲区大一个加密块，否则会出现错误。
+        注意，如果出现overlap错误，请检查输入和输出缓冲区是否分离，以及是否其长度是否满足第一个注意事项
+
+        5.1. 加密
+        int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl, const unsigned char *in, int inl)
+        返回值为1表示成功，返回值为0表示失败
+
+        参数	描述
+        ctx	    加密上下文对象
+        out	    保存输出结果（密文）的缓冲区
+        outl	接收输出结果长度的指针
+        in	    包含输入数据（明文）的缓冲区
+        inl	    输入数据的长度
+
+        5.2. 解密
+        int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl, const unsigned char *in, int inl)
+        执行解密的函数，参数和返回值和上述加密函数类似，只需要注意输入和输出不要混淆
+
+
+    6. 加解密尾部数据处理
+
+        6.1. 加密
+        int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl)
+        返回值为1表示成功，0表示失败。
+
+        该函数处理加密结果的尾部数据（比如填充段块），还可能输出一些密文数据，参数描述如下：
+        参数	描述
+        ctx	    加密上下文对象
+        out	    保存输出结果（密文）的缓冲区（注意这个指针要指向之前已经保存的加密数据的尾部）
+        outl	接收输出结果长度的指针
+
+        6.2. 解密
+        int EVP_DecryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl)
+        该函数处理解密结果的尾部数据，还可能输出一些明文数据，参数和返回值同上述加密尾部数据处理的函数类似，注意这个函数输出的是明文即可
+
+    7. 资源释放
+        在加解密操作完成后，对可能的密码缓冲区的清空，以及释放上下文对象，一般使用上下文处理中的
+        void EVP_CIPHER_CTX_free(EVP_CIPHER_CTX *ctx)
+        
+    8. 口令生成密钥(key derivation)
+        有时候我们需要使用口令来生成加密密钥，openssl推荐使用PBKDF2算法来进行这个操作，使用到的函数如下。
+
+        int PKCS5_PBKDF2_HMAC(const char *pass, int passlen,
+                   const unsigned char *salt, int saltlen, int iter,
+                   const EVP_MD *digest,
+                   int keylen, unsigned char *out);
+        返回值为1表示成功，0表示失败。
+
+        该函数使用PKKDF2算法利用口令生成指定长度的密钥，其参数描述如下：
+            参数	描述
+            pass	用于生成密钥的口令
+            passlen	口令的长度
+            salt	用于生成密钥的盐值(建议4字节以上)，当然也可以设置为NULL表示不使用
+            saltlen	盐值的长度，如果不使用则为0
+            iter	迭代次数（openssl建议设置到1000以上，用于增加暴力破解的难度）
+            digest	单向hash函数，在openssl/evp.h中定义了许多以算法命名的函数, 这些函数的返回值作为此参数使用，比如EVP_sha256()
+            keylen	输出的密钥的长度
+            out	    保存输出的密钥的缓冲区
+
+    """
     ciphers = {
         'aes-128-cbc': (16, 16),
         'aes-192-cbc': (24, 16),
@@ -214,6 +337,7 @@ class OpenSSLCrypto:
                     files = glob.glob(pat)
                     if files:
                         paths.extend(files)
+
         for path in paths:
             try:
                 lib = CDLL(path)
@@ -250,6 +374,7 @@ class OpenSSLCrypto:
             self.libcrypto.EVP_CIPHER_CTX_cleanup.argtypes = (c_void_p,)
         else:
             self.libcrypto.EVP_CIPHER_CTX_reset.argtypes = (c_void_p,)
+
         self.libcrypto.EVP_CIPHER_CTX_free.argtypes = (c_void_p,)
 
         self.libcrypto.RAND_bytes.restype = c_int
