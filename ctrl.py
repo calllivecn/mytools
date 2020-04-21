7c71565a6bb4661d8f2acc5d47bfccf20ad1e041
calllivecn <calllivecn@outlook.com>
2020-04-21 18:08:56 +0800

    writing...

--- ctrl.py
+++ ctrl.py
@@ -13,7 +13,9 @@
 import os
 import sys
 import subprocess
+import argparse
 import logging
+import threading
 import struct
 import socket
 import socketserver
@@ -30,7 +32,7 @@ def getlogger(level=logging.INFO):
     logger.addHandler(stream)
     return logger
 
-logger = getlogger()
+logger = getlogger(logging.DEBUG)
 
 def shell(cmd, timeout=5):
 
@@ -52,10 +54,10 @@ class Plugin:
     def run(self):
         pass
 
-bufsize = 1024
+bufsize = 2048
 
 PROTOCOL_VERSION = 0x01
-PROTCOL_HEADER = struct.Struct("!HHH")
+PROTOCOL_HEADER = struct.Struct("!HHH")
 """
 指令包装:
     version:    2byte,  
@@ -66,11 +68,11 @@ PROTCOL_HEADER = struct.Struct("!HHH")
 
 def recv_cmd(sock):
 
-    data, addr = sock.recvfrom(PROTCOL_HEADER.size)
+    data, addr = sock.recvfrom(bufsize)
 
-    version, cmd_number, cmd_len = PROTCOL_HEADER.unpack(data)
+    version, cmd_number, cmd_len = PROTOCOL_HEADER.unpack(data[PROTOCOL_HEADER.size])
 
-    cmd = sock.recvfrom(bufsize)
+    cmd = data[PROTOCOL_HEADER.size:cmd_len].decode()
     
     log = f"{addr} 接收到指令号：{cmd_number} -- "
 
@@ -87,9 +89,9 @@ def send_cmd(sock, cmd_number, cmd):
     cmd = cmd.encode()
     cmd_len = len(cmd)
 
-    data = PROTCOL_HEADER.pack(PROTOCOL_VERSION, cmd_number, cmd_len)
+    data = PROTOCOL_HEADER.pack(PROTOCOL_VERSION, cmd_number, cmd_len)
 
-    sock.sendto(PROTCOL_HEADER.pack())
+    sock.sendto(data + cmd, ("",6789))
     
     log = f"发送指令号：{cmd_number} -- "
 
@@ -99,3 +101,80 @@ def send_cmd(sock, cmd_number, cmd):
     logger.info(log)
 
     return cmd_number, cmd
+
+# client begin
+
+def broadcast_cmd(cmd_number, cmd):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    send_cmd(sock, cmd_number, cmd)
+
+# client end
+
+# server begin
+
+class subcmd(threading.Thread):
+    """
+    args: (shellcmd,)
+    """
+    def __init__(self, cmd):
+        super().__init__(self)
+        self.cmd = cmd
+
+    def run(self):
+        shell(self.cmd)
+
+
+class ThreadUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+    daemon_threads = True
+
+class UDPHandler(socketserver.DatagramRequestHandler):
+
+    def handle(self):
+        logger.info(f"client: {self.client_address}")
+        data = self.request[0]
+        sock = self.request[1]
+        logger.debug(f"{data} -- {sock}")
+
+def server():
+    #with ThreadUDPServer(("0.0.0.0", 6789), UDPHandler) as server:
+    with ThreadUDPServer(("", 6789), UDPHandler) as server:
+        server.request_queue_size = 128
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        server.socket = sock
+        server.serve_forever()
+
+# server end
+
+def parse_cli():
+    parse = argparse.ArgumentParser()
+
+    parse.add_argument("--server", action="store_true", help="start a server.")
+
+    parse.add_argument("-t", "--type", type=int, help="指令类型，1 为shell 指令， 0 不使用。")
+
+    parse.add_argument("cmd", nargs="?", help="如果是shell指令的话，必需存在。")
+
+    parse.add_argument("--parse", action="store_true", help="debug argparse parse_args()")
+
+    args = parse.parse_args()
+
+    if args.parse:
+        print(args)
+        sys.exit(0)
+
+    return args
+
+
+if __name__ == "__main__":
+    
+    args = parse_cli()
+
+    if args.server:
+        server()
+        sys.exit(0)
+
+    broadcast_cmd(args.type, args.cmd)
+
+    
