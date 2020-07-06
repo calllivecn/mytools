c530b31b1ca9cedd151ee0ed11d77d0c74525c0d
calllivecn <calllivecn@outlook.com>
2020-07-06 10:06:55 +0800

    修改到 url 为参数

--- git-clone.sh
+++ git-clone.sh
@@ -7,27 +7,30 @@ set -e
 PROGRAM=${0##*/}
 
 USAGE="\
-Usage: $PROGRAM <github username> <repository>
+Usage: $PROGRAM <github repository> [branch]
 "
 
-if [ -z "$1" ] || [ -z "$2" ];then
+if [ -z "$1" ];then
 	echo "$USAGE"
 	exit 1
 fi
 
-GITUSER="$1"
+GIT="$1"
 
-REPO="$2"
+BRANCH="$2"
 
-BRANCH="$3"
+eval $(python3 -c "from urllib import parse;p=parse.urlparse(\"$GIT\");print(f'scheme={p.scheme} host={p.netloc} path={p.path}')")
+
+tmp_str=${path%.git}
+REPO=$(basename "$tmp_str")
 
 if [ -n "$BRANCH" ];then
-	git clone -b "$BRANCH" "https://github.com.cnpmjs.org/${GITUSER}/${REPO}.git"
+	git clone -b "$BRANCH" "https://github.com.cnpmjs.org${path}"
 else
-	git clone "https://github.com.cnpmjs.org/${GITUSER}/${REPO}.git"
+	git clone "https://github.com.cnpmjs.org${path}"
 fi
 
 cd "${REPO}"
 
-git remote set-url origin "https://github.com/${GITUSER}/${REPO}.git"
+git remote set-url origin "https://github.com${path}"
 
