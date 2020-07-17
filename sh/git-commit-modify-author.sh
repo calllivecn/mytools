64de70ffd8ad8ac108a0510bde8ec7099af33065
calllivecn <calllivecn@outlook.com>
2020-07-17 11:37:12 +0800

    完善git-filter-branch

--- sh/git-commit-modify-author.sh
+++ sh/git-commit-modify-author.sh
@@ -2,21 +2,20 @@
 # date 2020-04-30 17:40:01
 # author calllivecn <c-all@qq.com>
 
+export FILTER_BRANCH_SQUELCH_WARNING=1
 
 git filter-branch --env-filter '
-OLD_EMAIL="your email"
+OLD_NAME="your old name"
+OLD_EMAIL="your ola email"
 
-CORRECT_NAME="new username"
-CORRECT_EMAIL="new email"
+NEW_NAME="your name"
+NEW_EMAIL="your email"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ] && [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ];then
+	echo "找到目标， 进行替换"
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+else
+	echo "没有找到目标， 不进行替换"
 fi
 ' --tag-name-filter cat -- --branches --tags
