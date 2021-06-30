4ff110c3a63a19286de7addaecf9a3f6f864f6b6
calllivecn <calllivecn@outlook.com>
2021-06-30 19:09:35 +0800

    添加默认输出到标准输出，添加--sha-all

--- wget.py
+++ wget.py
@@ -37,7 +37,7 @@ class Progress:
 
         while not self.__is_end():
             for c in ["-", "\\", "|", "/"]:
-                print(c*10, end="\r")
+                print(c*10, end="\r", file=sys.stderr)
                 time.sleep(0.2)
         #print("进度线程是正常结束的~")
 
@@ -53,8 +53,8 @@ class Wget:
 
     HASH = ("md5", "sha1", "sha224", "sha256", "sha384", "sha512")
 
-    def __init__(self, shafunc=[]):
-        self.shafuncnames = shafunc
+    def __init__(self, shafunc=set()):
+        self.shafuncnames = sorted(shafunc)
 
         self.shafuncs = []
 
@@ -63,7 +63,6 @@ class Wget:
             self.sha = False
         else:
             self.sha = True
-
 
         # 需要计算的hash函数
         for funcname in self.shafuncnames:
@@ -78,8 +77,8 @@ class Wget:
             func.update(data)
     
     def print(self):
-        for index, func in enumerate(self.shafuncs):
-            print(f"{self.shafuncnames[index]}: {func.hexdigest()}")
+        for func in self.shafuncs:
+            print(f"{func.name}: {func.hexdigest()}", file=sys.stderr)
 
 
     def wget(self, savepath, url):
@@ -89,13 +88,17 @@ class Wget:
         req = request.Request(url, headers=headers)
         response = request.urlopen(req, timeout=15)
 
-        with open(savepath, "wb") as f:
+        if savepath == "-":
             for data in iter(partial(response.read, block), b""):
-                f.write(data)
-
+                sys.stdout.buffer.write(data)
                 if self.sha:
                     self.update(data)
-
+        else:
+            with open(savepath, "wb") as f:
+                for data in iter(partial(response.read, block), b""):
+                    f.write(data)
+                    if self.sha:
+                        self.update(data)
         self.print()
 
 
@@ -104,37 +107,46 @@ def main():
 
     parse = argparse.ArgumentParser(usage="%(prog)s [optional] <url>")
 
-    parse.add_argument("-o", required=True, help="保存文件名")
+    parse.add_argument("-o", default="-", help="保存文件名")
     parse.add_argument("--md5", action="store_true", help="下载同时计算 md5")
     parse.add_argument("--sha1", action="store_true", help="下载同时计算 sha1")
     parse.add_argument("--sha224", action="store_true", help="下载同时计算 sha224")
     parse.add_argument("--sha256", action="store_true", help="下载同时计算 sha256")
     parse.add_argument("--sha384", action="store_true", help="下载同时计算 sha384")
     parse.add_argument("--sha512", action="store_true", help="下载同时计算 sha512")
+    parse.add_argument("--sha-all", action="store_true", help="下载同时计算以上所有值")
+    parse.add_argument("--parse", action="store_true", help=argparse.SUPPRESS)
 
     parse.add_argument("url", help="下载链接")
 
     args = parse.parse_args()
 
-    shafuncs = []
+    if args.parse:
+        print(args)
+        sys.exit(0)
+
+    shafuncs = set()
 
     if args.md5:
-        shafuncs.append("md5") 
+        shafuncs |= {"md5"}
 
     if args.sha1:
-        shafuncs.append("sha1") 
+        shafuncs |= {"sha1"}
 
     if args.sha224:
-        shafuncs.append("sha224") 
+        shafuncs |= {"sha224"}
 
     if args.sha256:
-        shafuncs.append("sha256") 
+        shafuncs |= {"sha256"}
 
     if args.sha384:
-        shafuncs.append("sha384") 
+        shafuncs |= {"sha384"}
 
     if args.sha512:
-        shafuncs.append("sha512") 
+        shafuncs |= {"sha512"}
+    
+    if args.sha_all:
+        shafuncs |= set(Wget.HASH)
 
     w = Wget(shafuncs)
 
