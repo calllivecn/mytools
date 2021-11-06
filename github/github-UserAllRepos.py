7692e472be0aa347a8ab38b9be2ec9255ddb22a8
calllivecn <calllivecn@outlook.com>
2021-11-06 16:18:02 +0800

    添加使用token，查看所有仓库(有私仓)

--- github/github-UserAllRepos.py
+++ github/github-UserAllRepos.py
@@ -4,28 +4,81 @@
 # author calllivecn <c-all@qq.com>
 
 
+import os
 import sys
 import json
 from urllib import request
 
+"""
+文档：https://docs.github.com/cn/rest/reference/repos#list-repositories-for-a-user
+"""
+
+# github 默认页大小为：30, MAX: 100
+per_page=100
 
 
+def findnext(link):
 
-GITHUB_USER_REPOS="https://api.github.com/users/{}/repos"
+    if link is None:
+        return None
 
-
-def get_all_repos(username):
+    for l in link.split(","):
+        if l.find("next") != -1:
+            url = l.split(";")[0]
+            url = url.strip()
+            return url.strip("<>")
     
-    url = GITHUB_USER_REPOS.format(username)
-    req = request.Request(url, headers={"Accept": "application/vnd.github.nebula-preview+json"})
-    data = request.urlopen(req).read()
+    return None
 
-    for repo in json.loads(data):
+
+def get_request(url, headers):
+    req = request.Request(url, headers=headers)
+    return request.urlopen(req)
+
+def build_request(username_or_token):
+
+    url =f"https://api.github.com/users/{username_or_token}/repos?per_page={per_page}"
+
+    # headers = {"Accept": "application/vnd.github.nebula-preview+json"}
+    headers = {"Accept": "application/vnd.github.v3+json"}
+
+    # 是使用的token
+    if username_or_token.startswith("ghp_") and len(username_or_token) == 40:
+        headers.update({"Authorization": f"Token {username_or_token}"})
+        url = f"https://api.github.com/user/repos?per_page={per_page}"
+
+    return url, headers
+
+
+def get_all_repos(username_or_token):
+
+    link_next, headers = build_request(username_or_token)
+    jdata = []
+    while True:
+
+        print("link_next:", link_next, file=sys.stderr)
+        result = get_request(link_next, headers)
+
+        jdata += json.loads(result.read())
+
+        link = result.getheader("Link")
+
+        # debug
+        # print("debug: ", link)
+
+        # 拿到下一页的link
+        link_next = findnext(link)
+        if link_next is None:
+            break
+
+    # output
+    for repo in jdata:
         print(repo["clone_url"])
 
 
 USAGE="""\
-Usage: {} <github username>
+Usage: {} <github username|github token>
+# 使用 github token 可以查看到私人仓库
 """.format(sys.argv[0])
 
 def usage():
