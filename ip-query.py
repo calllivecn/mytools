df673855f80b332333230c1050a91fa3bbc4a52a
calllivecn <c-all@qq.com>
2022-02-18 13:05:59 +0800

    update

--- ip-query.py
+++ ip-query.py
@@ -1,52 +1,79 @@
 #!/usr/bin/env python3
 # coding=utf-8
+# update: 2022-02-18 12:50
 
+import sys
 import json
+import argparse
+import ipaddress
 from urllib.request import urlopen
 from socket import gethostbyname_ex
-import sys
-import argparse
+
+def getselfip():
+    url = "https://www.taobao.com/help/getip.php"
+    result = urlopen(url).read()
+    _, ip, _ = result.decode("utf-8").split('"')
+    print(ip)
+    return ip
+    
 
 
 def query(ipaddr):
     # 地址
-    url = 'http://ip.taobao.com/service/getIpInfo.php?ip={}'.format(ipaddr)
+    url = f"https://ip.taobao.com/service/getIpInfo.php?ip={ipaddr}"
+    url = f"https://ip.taobao.com/getIpInfo.php?ip={ipaddr}"
     response = urlopen(url)  # post提交数据
 
     html = response.read()  # 接收返回数据
 
-    tag = json.loads(html.decode())  # ,encoding='utf8') #josh格式转换
+    tag = json.loads(html.decode("utf-8"))  # ,encoding='utf8') #josh格式转换
 
-    if tag["code"] == 0:
-        data = tag["data"]
+    if tag.get("code") == 0 or tag.get("code") == "0":
+        #data = tag["data"]
+        print(json.dumps(tag["data"], ensure_ascii=False, indent=4))
     else:
         print('返回码非零,查询出错。', ipaddr)
-        return 1
+        print(json.dumps(tag, ensure_ascii=False, indent=4))
 
     # print(data)
 
-    ip = data["ip"]  # 要查询IP
-    city = data["city"]  # 城市
-    region = data["region"]  # 省
-    area = data["area"]  # 区域
-    isp = data["isp"]  # ISP
-    county = data["county"]  # 市区
-    country = data["country"]  # 国家
-    return "\tIP:{}\n\t运营商：{}\n\t国家:{}\n\t地区城市:{}{}".format(ip, isp, country, region, city)
+    #ip = data["ip"]  # 要查询IP
+    #city = data["city"]  # 城市
+    #region = data["region"]  # 省
+    #area = data["area"]  # 区域
+    #isp = data["isp"]  # ISP
+    #county = data["county"]  # 市区
+    #country = data["country"]  # 国家
+    #return "\tIP:{}\n\t运营商：{}\n\t国家:{}\n\t地区城市:{}{}".format(ip, isp, country, region, city)
 
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser(
-        description='query ip information', epilog='this description end.', add_help=True)
+        description='使用淘宝API查询ip地址信息', epilog='calllivecn')
 
     #parse.add_argument('-a','--address',nargs="*",help="addresss or hostnames")
-    parse.add_argument('address', nargs="*", help="addresss or hostnames")
+    parse.add_argument('address', nargs="*", help="IP地址 或者 域名")
 
     args = parse.parse_args()
 
-    for ip_or_hostname in args.address:
-        print('查询：', ip_or_hostname)
-        ip_lists = gethostbyname_ex(ip_or_hostname)
-        for ip in ip_lists[2]:
-            print('\n', query(ip), sep='')
-        print()
+    if args.address:
+
+        for ip_or_hostname in args.address:
+
+            try:
+                ipaddress.ip_address(ip_or_hostname)
+            except ValueError:
+                # 不是ip , 当域名查询
+                ip_lists = gethostbyname_ex(ip_or_hostname)
+                print(ip_lists)
+                for ip in ip_lists[2]:
+                    print('查询：', ip)
+                    query(ip)
+                print()
+            else:
+                print('查询：', ip_or_hostname)
+                query(ip_or_hostname)
+                print()
+
+    else:
+        getselfip()
