b1e61dd59927bb987b0cdb43c0780f4158de488c
calllivecn <c-all@qq.com>
2022-08-18 11:15:44 +0800

    找到调节外接显示器亮度的工具(不止调节亮度)

---
+++ adjbacklight-ext.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# date 2021-10-17 20:42:56
+# author calllivecn <c-all@qq.com>
+
+# X11 下的
+# SELECT_MONITOR: 外接显示器(使用randr, 命令查看), $1: 0 ~ 1 之间的值，亮度。
+# xrandr --output "$SELECT_MONITOR" --brightness "$1"
+
+# 调整外接显示的亮度, 用户需要在 i2c 用户组里, 否则需要root执行。
+# apt install ddcutil
+# ddcutil getvcp 0x10: --> VCP code 0x10 (Brightness): current value =    17, max value =   100
+
+if groups |grep -q i2c;then
+	ddcutil getvcp 0x10
+	ddcutil setvcp 0x10 "$1"
+else
+	sudo ddcutil setvcp 0x10 "$1"
+fi
+
