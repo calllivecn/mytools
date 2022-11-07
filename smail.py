b52866f41bc9d4e1c4756fb53a8d0a6d0118e056
calllivecn <calllivecn@outlook.com>
2022-11-07 13:40:51 +0800

    初步测试能用，行这么着。

--- smail.py
+++ smail.py
@@ -1,119 +1,179 @@
 #!/usr/bin/env python3
 # coding=utf-8
+# update 2022-11-07 11:58
 
 
 import os
 import sys
-import smtplib
 import base64
+import smtplib
 import argparse
+import configparser
+from pathlib import Path
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-# ---------------------------------------------------------------
 # config information
-server = 'smtp.qq.com'
-username = ''
-password = ''
+CFG="""\
+[Smtp]
+Server = smtp.qq.com
+Email =
+Password =
+"""
 
-# ----------------------------------------------------------------
+HOME = os.environ.get("HOME")
 
-parse = argparse.ArgumentParser(description="%(prog)s is a cmdline smtp mail")
-
-parse.add_argument('-T', '--to', nargs='+', required=True,
-                   help='to mail address list')
-
-parse.add_argument(
-    '-t', '--text', default='this is a test mail.', help='mail test')
-
-parse.add_argument('-u', '--user', default=username, help='mail user')
-
-parse.add_argument('-p', '--passwd', default=password, help='mail password')
-
-parse.add_argument('-s', '--subject',
-                   default='known subject', help='mail content')
-
-# parse.add_argument('-F','--From',default='qq@qq.com',help='Nothing')
-parse.add_argument('-F', '--From', help='Nothing')
-
-parse.add_argument('-f', '--file', help='mail content text file')
-
-parse.add_argument('-a', '--attach', nargs='+', help='mail attach')
-
-parse.add_argument('-v', '--verbose', action='count',
-                   default=0, help='verbose')
-
-
-args = parse.parse_args()
-
-# print(args)
-
-msg = MIMEMultipart()
-
-if args.From:
-    msg['From'] = args.From
+if HOME:
+    CONF = Path(HOME) / ".config/smail.conf"
 else:
-    msg['From'] = args.user
+    CONF = Path(".config/smail.conf")
 
-msg['Subject'] = args.subject
+def readcfg(filename, init_context=None):
+    conf = configparser.ConfigParser()
+    if filename.exists():
+        conf.read(str(filename))
+        return conf
+    else:
+        if init_context is None:
+            raise Exception(f"初始化配置文本没有提供: {init_context}")
+        else:
+            with open(filename, "w") as fp:
+                fp.write(init_context)
 
-if args.file:
-    with open(args.file) as f:
-        text = '#### Email send content filename :' + f.name + '\n\n'
-        text += f.read()
-    args.text = text
-
-if args.attach:
-    for a in args.attach:
-
-        basename = os.path.basename(a)
-
-        with open(a, 'rb') as fp:
-            att = MIMEText(fp.read(), 'base64', 'utf-8')
-
-        att["Content-Type"] = 'application/octet-stream'
-        att.add_header('Content-Disposition', 'attachment',
-                       filename=('utf-8', '', basename))
-        # encoders.encode_base64(att)
-        msg.attach(att)
-
-content1 = MIMEText(args.text, 'plain', 'utf-8')
-msg.attach(content1)
+            conf.read_string(init_context)
+            return conf
 
 
 # -----------------------------------------------------------
-try:
-    s = smtplib.SMTP_SSL(server)
+def send(server, email, passwd, to, msg, verbose):
+    try:
+        s = smtplib.SMTP_SSL(server)
 
-    if args.verbose:
-        s.set_debuglevel(args.verbose)
+        if verbose:
+            s.set_debuglevel(verbose)
 
-    # s.connect(server)
-    #s.esmtp_features["auth"]="LOGIN PLAIN"
+        # s.connect(server)
+        #s.esmtp_features["auth"]="LOGIN PLAIN"
 
-    code = s.ehlo()[0]
-    usesesmtp = True
-    if not (200 <= code <= 299):
-        usesesmtp = False
-        code = helo()[0]
+        code = s.ehlo()[0]
+        usesesmtp = True
         if not (200 <= code <= 299):
-            raise smtplib.SMTPHeloError
-    if usesesmtp and s.has_extn('size'):
-        if len(msg) > int(s.esmtp_features['size']):
-            print('Maximum message size is {}MB'.format(
-                int(s.esmtp_features['size']) / (1 << 20)))
-            print('Message too large ; aborting.')
-            sys.exit(2)
+            usesesmtp = False
+            code = s.helo()[0]
+            if not (200 <= code <= 299):
+                raise smtplib.SMTPHeloError
+        if usesesmtp and s.has_extn("size"):
+            if len(msg) > int(s.esmtp_features["size"]):
+                print("Maximum message size is {}MB".format(
+                    int(s.esmtp_features["size"]) / (1 << 20)))
+                print("Message too large ; aborting.")
+                sys.exit(2)
 
-    # s.starttls()
-    # s.helo()
-    s.login(args.user, args.passwd)
-    if s.sendmail(args.user, args.to, msg.as_string()):
-        print('Recv : error.')
-except (smtplib.SMTPException, smtplib.SMTPHeloError) as e:
-    print('SMTPException ', e)
-    sys.exit(1)
-finally:
-    #    s.close()
-    s.quit()
+        # s.starttls()
+        # s.helo()
+        # s.login(args.user, args.passwd)
+        s.login(email, passwd)
+        if s.sendmail(email, to, msg.as_string()):
+            print("Recv : error.")
+
+    except (smtplib.SMTPException, smtplib.SMTPHeloError) as e:
+        print("SMTPException ", e)
+        sys.exit(1)
+
+    finally:
+        #    s.close()
+        s.quit()
+
+
+
+def main():
+    parse = argparse.ArgumentParser(description="%(prog)s SMTP mail 发送工具")
+
+    parse.add_argument("-c", "--conf", default=CONF, help=f"配置(default: ~/.config/smail.conf")
+
+    # parse.add_argument("-u", "--user", help="mail user")
+    # parse.add_argument("-p", "--passwd", help="mail password")
+
+    parse.add_argument("-T", "--to", nargs="+", required=True, help="发送给谁, 可以多个地址。")
+    parse.add_argument("-s", "--subject", default="unknown subject", help="邮件主题")
+
+    # text = parse.add_argument_group(title="文件内容选项")
+    text = parse.add_mutually_exclusive_group()
+    text.add_argument( "-t", "--text", default="%(prog)s 工具默认邮件内容", help="邮件内容")
+    text.add_argument("--text-infile", dest="infile", type=Path, help="从一个文体文件读取内容")
+    text.add_argument("--text-stdin", dest="stdin", help="从一个文体文件读取内容(Max: 8K")
+
+    parse.add_argument("-F", "--From", help="Mail From.")
+
+    parse.add_argument("-a", "--attach", nargs="+", help="邮件附件")
+
+    parse.add_argument("-v", "--verbose", action="count", default=0, help="verbose")
+
+    parse.add_argument("--parse", action="store_true", help=argparse.SUPPRESS)
+
+
+    args = parse.parse_args()
+
+    if args.parse:
+        print(args)
+        sys.exit(0)
+    
+    # conf 
+    cfg = readcfg(CONF, CFG)
+
+    try:
+        config = cfg["Smtp"]
+        server = config["Server"]
+        email = config["Email"]
+        password = config["Password"]
+        print(locals())
+    except Exception:
+        print("需要配置:")
+        print(CFG)
+        sys.exit(1)
+
+    msg = MIMEMultipart()
+
+    if args.From:
+        msg["From"] = args.From
+    else:
+        msg["From"] = email
+
+    msg["Subject"] = args.subject
+    
+
+    if args.text:
+        Text = args.text
+
+    elif args.infile:
+        with open(args.infile) as f:
+            text = "#### Email send content filename: " + f.name + " ####\n\n"
+            text += f.read()
+
+        Text = text
+
+    elif args.stdin:
+        Text = sys.stdin.read(8192)
+
+    content1 = MIMEText(Text, "plain", "utf-8")
+
+    if args.attach:
+        for a in args.attach:
+            p = Path(a)
+            with open(a, "rb") as fp:
+                att = MIMEText(fp.read(), "base64", "utf-8")
+
+            att["Content-Type"] = "application/octet-stream"
+            att.add_header("Content-Disposition", "attachment", filename=("utf-8", "", p.name))
+            # encoders.encode_base64(att)
+            msg.attach(att)
+
+    msg.attach(content1)
+
+    send(server, email, password, args.to, msg, args.verbose)
+
+
+
+if __name__ == "__main__":
+    main()
