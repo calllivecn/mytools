087803ea8d48c106b0fe43e65d2aa03010565c55
calllivecn <c-all@qq.com>
2023-03-28 22:14:00 +0800

    试验py 查看和 tail -f journal日志

---
+++ sh/journal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2023-03-28 21:00:56
+# author calllivecn <c-all@qq.com>
+
+from systemd import journal
+
+
+j = journal.Reader()
+
+#j.add_match(_COMM="systemd-logind")
+
+j.seek_tail()
+#j.get_previous()
+
+
+while True:
+    event = j.wait() # 返回NOP（无更改）、APPEND（新条目已添加到 日志结束）或INVALIDATE（已添加日志文件或 移除）
+    if event == journal.APPEND:
+        #l = j.get_next()
+        #print("line:", l)
+        for l in j:
+            print("line:", l)
