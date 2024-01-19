4020a814915e98f64baef1d2d06d32956ff175cb
calllivecn <calllivecn@outlook.com>
2024-01-19 12:42:10 +0800

    save

--- github/rewrite-commit-msg.sh
+++ github/rewrite-commit-msg.sh
@@ -13,15 +13,4 @@ then
     export GIT_COMMITTER_EMAIL="new_email_address@example.com"
 fi
 ' --tag-name-filter cat -- --branches --tags
-t filter-branch --env-filter '
-if [ "$GIT_AUTHOR_EMAIL" = "old_email_address@example.com" ]
-then
-    export GIT_AUTHOR_EMAIL="new_email_address@example.com"
-fi
-if [ "$GIT_COMMITTER_EMAIL" = "old_email_address@example.com" ]
-then
-    export GIT_COMMITTER_EMAIL="new_email_address@example.com"
-fi
-' --tag-name-filter cat -- --branches --tags
-
 
