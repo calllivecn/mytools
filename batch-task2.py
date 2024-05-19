9d369a54e05ed3b0aaf473c7575667493540d35e
calllivecn <calllive@outlook.com>
2024-05-19 11:20:21 +0800

    fix 小问题

--- batch-task2.py
+++ batch-task2.py
@@ -369,7 +369,7 @@ def server(args):
             proto = pickle.loads(data)
             # print("type:", proto[0])
         except Exception:
-            logger.deubg(f"接收客户端数据异常")
+            logger.debug(f"接收客户端数据异常")
             traceback.print_exc()
             continue
 
