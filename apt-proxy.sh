d9a2073373555c34335516f44bfb37ef781c4e95
calllivecn <calllive@outlook.com>
2025-04-20 10:21:36 +0800

    调整使用xiyou

--- apt-proxy.sh
+++ apt-proxy.sh
@@ -3,11 +3,10 @@
 # author calllivecn <calllivecn@outlook.com>
 
 
+#PROXY_ADDR="$(grep -oP '(?<=http_proxy=")(.*)(?=")' $HOME/.config/xiyou-s801)"
 PROXY_ADDR="$(grep -oP '(?<=http_proxy=")(.*)(?=")' $HOME/.config/xiyou)"
 
 URLs="ppa.launchpad.net
-linux.teamviewer.com
-*.opensuse.org
 packages.microsoft.com
 dl.google.com"
 
