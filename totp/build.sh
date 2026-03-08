6c6f465d45ead36817f8ed414959a288140dd3ea
calllivecn <calllive@outlook.com>
2026-03-08 14:50:03 +0800

    更新totp

--- totp/build.sh
+++ totp/build.sh
@@ -1,7 +1,14 @@
 
-cp -v $(type -p crypto.py) .
+if [ -f crypto.py ];then
+    :
+else
+    echo "需要cp -v crypto.py 到当前目录"
+    exit 1
+fi
+
+#cp -v $(type -p crypto.py) .
 
 podman build -t totp:$(date +%F) .
 
-rm -v crypto.py
+#rm -v crypto.py
 
