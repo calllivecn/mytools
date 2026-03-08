df55b4d964bd4cae2933d6279e0279f635d505f3
calllivecn <calllive@outlook.com>
2026-03-08 15:01:07 +0800

    更新totp的加密:crypto.py到新版

--- totp/build.sh
+++ totp/build.sh
@@ -1,14 +1,15 @@
 
-if [ -f crypto.py ];then
-    :
+CTYPTO="$1"
+
+if [ -f $CRYPTO ];then
+    cp -v $CRYPTO .
 else
     echo "需要cp -v crypto.py 到当前目录"
     exit 1
 fi
 
-#cp -v $(type -p crypto.py) .
 
 podman build -t totp:$(date +%F) .
 
-#rm -v crypto.py
+rm -v crypto.py
 
