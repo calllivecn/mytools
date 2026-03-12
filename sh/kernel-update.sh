88c881c849bd726827805d239fb8885a33d0d0ac
calllivecn <calllivecn@outlook.com>
2026-03-12 08:42:42 +0800

    update

--- sh/kernel-update.sh
+++ sh/kernel-update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # date 2018-11-24 17:26:43
-# author calllivecn <calllivecn@outlook.com>
+# author calllivecn <c-all@qq.com>
 
 
 set -xe
 
-KERNEL="4.18.0"
-KERNEL="4.15.0"
-KERNEL="5.3.0"
+KERNEL="5.11.0"
+KERNEL="5.15.0"
+KERNEL="5.19.0"
+KERNEL="6.2.0"
+KERNEL="6.5.0"
+KERNEL="6.8.0"
+KERNEL="6.11.0"
+KERNEL="6.14.0"
+KERNEL="6.17.0"
 
 if echo "$1" | grep -qE '[0-9]{2,3}';then
 	kernel_version="$1"
@@ -17,9 +23,9 @@ else
 fi
 
 if [ "$2"x == "update"x ];then
-	sudo apt install linux-image-${KERNEL}-${kernel_version}-generic linux-modules-${KERNEL}-${kernel_version}-generic linux-modules-extra-${KERNEL}-${kernel_version}-generic linux-headers-${KERNEL}-"${kernel_version}"{,-generic}
+	sudo apt install linux-image-${KERNEL}-${kernel_version}-generic linux-modules-${KERNEL}-${kernel_version}-generic linux-modules-extra-${KERNEL}-${kernel_version}-generic linux-headers-${KERNEL}-"${kernel_version}"-generic
 elif [ "$2"x == "remove"x ];then
-	sudo apt autoremove --purge linux-image-${KERNEL}-${kernel_version}-generic linux-modules-${KERNEL}-${kernel_version}-generic linux-modules-extra-${KERNEL}-${kernel_version}-generic linux-headers-${KERNEL}-"${kernel_version}"{,-generic}
+	sudo apt autoremove --purge linux-image-${KERNEL}-${kernel_version}-generic linux-modules-${KERNEL}-${kernel_version}-generic linux-modules-extra-${KERNEL}-${kernel_version}-generic linux-headers-${KERNEL}-"${kernel_version}"-generic
 else
-	sudo apt install linux-image-${KERNEL}-${kernel_version}-generic linux-modules-${KERNEL}-${kernel_version}-generic linux-modules-extra-${KERNEL}-${kernel_version}-generic linux-headers-${KERNEL}-"${kernel_version}"{,-generic}
+	sudo apt install linux-image-${KERNEL}-${kernel_version}-generic linux-modules-${KERNEL}-${kernel_version}-generic linux-modules-extra-${KERNEL}-${kernel_version}-generic linux-headers-${KERNEL}-"${kernel_version}"-generic
 fi
